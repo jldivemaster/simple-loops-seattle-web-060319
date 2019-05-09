90273e202733d978e25fe1dd2bc00134667dd4c0
Jason Leach <jlspursfan@gmail.com>
2019-05-09 01:10:43 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -33,7 +33,7 @@ end
 def until_iterator(number_of_times)
   counter = 0
 
-  until counter = number_of_times
+  until counter == number_of_times
     phrase = "Welcome to Flatiron School's Web Development Course!"
     puts phrase
     counter += 1
