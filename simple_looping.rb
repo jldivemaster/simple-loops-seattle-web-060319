fc355b09ba0b971fabeedba39093bd2edf360295
Jason Leach <jlspursfan@gmail.com>
2019-05-09 01:10:23 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -32,9 +32,9 @@ end
 
 def until_iterator(number_of_times)
   counter = 0
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-
+  
   until counter = number_of_times
+    phrase = "Welcome to Flatiron School's Web Development Course!"
     puts phrase
     counter += 1
   end
